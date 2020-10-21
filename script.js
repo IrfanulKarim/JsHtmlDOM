385e847cbebf2f0752fb28d1a183a042a8131cf2
Irfanul Karim <karimirfanul10@gmail.com>
2020-10-21 19:58:49 +0600

    DOM html

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+//////////////////////////////// DOM Element
+
 // var x = document.querySelectorAll("p.red");
 // document.getElementById("demo").innerHTML = x[0].innerHTML;
 
-function submit(){
-    var x = document.forms["form1"];
+// function submit(){
+//     var x = document.forms["form1"];
 
-    var text = "";
-    var i;
+//     var text = "";
+//     var i;
     
-    for(i=0; i<x.length; i++){
-        text += x.elements[i].value+ "</br>";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
+//     for(i=0; i<x.length; i++){
+//         text += x.elements[i].value+ "</br>";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+////////////////////////////////// DOM HTML
+
+document.getElementById("im1").src= "./images/logo.png";
