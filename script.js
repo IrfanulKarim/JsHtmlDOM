a919759b0cee1474492f03c492b4b5d732ae7a40
Irfanul Karim <karimirfanul10@gmail.com>
2020-10-21 20:49:19 +0600

    DOM events

--- script.js
+++ script.js
@@ -22,19 +22,27 @@
 
 ////////////////////////////////// DOM animation
 
-function myMove(){
-    var elem = document.getElementById("animation");
-    var pos = 0;
-    var id = setInterval(frame, 5);
+// function myMove(){
+//     var elem = document.getElementById("animation");
+//     var pos = 0;
+//     var id = setInterval(frame, 5);
 
-    function frame(){
-        if(pos==350){
-            clearInterval(id);
-        }
-        else{
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
-    }
+//     function frame(){
+//         if(pos==350){
+//             clearInterval(id);
+//         }
+//         else{
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + "px";
+//         }
+//     }
+// }
+
+////////////////////////////////// DOM events
+
+function uppercase(){
+    var x = document.getElementById("name");
+
+   x.value =  x.value.toUpperCase();
 }
